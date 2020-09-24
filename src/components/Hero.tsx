386e80fd7638a0283375f172f776d8852a1c5048
heinerbehrends <heinerbehrends@hotmail.com>
2020-09-24 16:28:27 +0200

    Added scroll animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,48 +1,52 @@
 /** @jsx jsx */
-import { jsx, Box, Flex } from "theme-ui"
+import { jsx, Box, Flex, Styled } from "theme-ui"
 import React from "react"
-import { motion } from "framer-motion"
+import { motion, useViewportScroll, useTransform } from "framer-motion"
 import Greyhound from "./Greyhound"
 import Button from "./Button"
 import HeroParagraph from "./HeroParagraph"
 
 export default function Hero() {
+  const { scrollYProgress } = useViewportScroll()
+  const xPos = useTransform(scrollYProgress, [0, 0.5], [0, 1200])
+  const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0])
   return (
     <Box
       sx={{
         maxWidth: "960px",
         marginX: "auto",
+        paddingY: "5",
       }}
     >
-      <h1
-        sx={{
-          fontSize: "7",
-          textAlign: "center",
-          fontWeight: "thin",
-          lineHeight: "heading",
-        }}
-      >
-        Supersnelle webapps voor iedereen
-      </h1>
+      <Styled.h1>Supersnelle webapps voor iedereen</Styled.h1>
 
-      <Flex sx={{ alignItems: "center", justifyContent: "center" }}>
-        <motion.div animate={{ x: 500 }} transition={{ duration: 2 }}>
-          <Greyhound />
+      <Flex
+        sx={{ alignItems: "center", justifyContent: "center", marginTop: "3" }}
+      >
+        <motion.div
+          animate={{ x: [0, 0, 700] }}
+          transition={{ times: [0, 0.3, 1], duration: 3, ease: "easeOut" }}
+        >
+          <motion.div style={{ x: xPos }}>
+            <Greyhound />
+          </motion.div>
         </motion.div>
         <motion.div
           animate={{ opacity: 1 }}
           transition={{ duration: 2 }}
           sx={{
-            maxWidth: "360px",
+            maxWidth: "400px",
             lineHeight: "body",
             opacity: "0",
           }}
         >
-          <HeroParagraph />
+          <motion.div style={{ opacity: opacity }}>
+            <HeroParagraph />
+          </motion.div>
         </motion.div>
       </Flex>
-      <Box sx={{ textAlign: "center", marginTop: "4" }}>
-        <Button text="Nieuwsgierig?" location="/contact" />
+      <Box sx={{ textAlign: "center", marginTop: "5" }}>
+        <Button text="NIEUWSGIERIG?" location="/contact" />
       </Box>
     </Box>
   )
