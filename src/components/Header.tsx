c4b50df2b189d1fd0d229fedaf8d228a203fd968
heinerbehrends <heinerbehrends@hotmail.com>
2020-10-01 16:11:21 +0200

    tweaked flex style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,15 +19,13 @@ export default function Header() {
         height: "64px",
       }}
     >
-      <Flex sx={{ alignItems: "center" }}>
+      <Flex sx={{ alignItems: "center", flex: "1 1 auto" }}>
         <h4
           sx={{
             fontWeight: "light",
             fontStyle: "italic",
-            fontSize: ["5", "40px"],
+            fontSize: ["5", "5", "40px"],
             margin: "1",
-            alignItems: "center",
-            flex: "2 2 auto",
           }}
         >
           flyfi
@@ -45,7 +43,7 @@ export default function Header() {
       </Flex>
       <Flex
         sx={{
-          flex: "1 1 auto",
+          flex: "0 1 auto",
           justifyContent: "center",
         }}
       >
@@ -54,8 +52,8 @@ export default function Header() {
             display: ["none", "inherit"],
             alignItems: "center",
             backgroundColor: "background",
-            height: "124px",
-            width: "124px",
+            height: ["82px", "82px", "124px"],
+            width: ["82px", "82px", "124px"],
             borderRadius: "50%",
             boxShadow: "big",
           }}
@@ -63,22 +61,20 @@ export default function Header() {
           <Logo />
         </Flex>
       </Flex>
-      <nav>
-        <ul
-          sx={{
-            display: "flex",
-            flex: "2 2 auto",
-            padding: "0",
-            justifyContent: "flex-end",
-            height: "100%",
-            margin: "0",
-          }}
-        >
-          <TopBarNavItem text="faq" location="/faq" />
-          <TopBarNavItem text="about" location="/about" />
-          <TopBarNavItem text="contact" location="/contact" />
-          <TopBarNavItem text="home" location="/" variant="active" />
-        </ul>
+      <nav
+        sx={{
+          display: "flex",
+          flex: "1 1 auto",
+          padding: "0",
+          justifyContent: "flex-end",
+          height: "100%",
+          margin: "0",
+        }}
+      >
+        <TopBarNavItem text="faq" location="/faq" />
+        <TopBarNavItem text="about" location="/about" />
+        <TopBarNavItem text="contact" location="/contact" />
+        <TopBarNavItem text="home" location="/" variant="active" />
       </nav>
     </header>
   )
