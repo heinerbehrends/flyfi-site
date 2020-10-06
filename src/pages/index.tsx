6bf75fcdb889923a525a5408340d28c1d547a0b9
heinerbehrends <heinerbehrends@hotmail.com>
2020-10-06 16:28:16 +0200

    added main and footer to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import Header from "../components/Header"
 import Hero from "../components/Hero"
+import Main from "../components/Main"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Main />
+      <Footer />
     </>
   )
 }
