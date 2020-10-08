48a5465827785ae043d7c1795a3adbd6beb0cfd0
heinerbehrends <heinerbehrends@hotmail.com>
2020-10-08 15:25:54 +0200

    Moved header and footer to Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import React from "react"
-import Header from "../components/Header"
+import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Main from "../components/Main"
-import Footer from "../components/Footer"
 
 export default function Home() {
   return (
-    <>
-      <Header />
+    <Layout>
       <Hero />
       <Main />
-      <Footer />
-    </>
+    </Layout>
   )
 }
