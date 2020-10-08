9ecec969e26f51e48db724c06e3e989986b8fc34
heinerbehrends <heinerbehrends@hotmail.com>
2020-10-08 15:25:16 +0200

    Added animation to headline

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,68 +1,48 @@
 /** @jsx jsx */
 import { jsx, Grid } from "theme-ui"
 import React from "react"
-import Card from "./Card"
+import { useInView } from "react-intersection-observer"
+import { motion, useViewportScroll, useTransform } from "framer-motion"
+import CardsVoordelen from "./Cards-voordelen"
 
 export default function Main() {
+  const [ref, inView] = useInView({
+    rootMargin: "-100px 0px",
+    triggerOnce: true,
+  })
+  const [ref2, inView2] = useInView({
+    rootMargin: "-100px 0px",
+    triggerOnce: true,
+  })
+  console.log(inView)
   return (
     <React.Fragment>
-      <h2
+      <motion.h2
+        ref={ref}
+        animate={{ x: inView ? 0 : -800 }}
         sx={{
+          marginTop: ["5"],
           fontSize: ["5", "6"],
-          marginX: "3",
-          fontWeight: "700",
-          textAlign: "center",
+          marginX: ["3", "3", "4", "5"],
+          fontWeight: "900",
         }}
       >
-        De voordelen van <i>flyfi</i> webapps
-      </h2>
-      <Grid
+        De voordelen van <i>flyfi</i> webapps {inView}
+      </motion.h2>
+      <CardsVoordelen />
+      <motion.h3
+        ref={ref2}
+        animate={{ x: inView2 ? 0 : -800 }}
         sx={{
-          gridTemplateColumns: "repeat(auto-fill, minmax(35ch, 1fr))",
-          paddingX: ["0", "3", "4"],
-          maxWidth: "1280px",
-          marginX: "auto",
+          marginTop: ["5"],
+          fontSize: ["4", "5"],
+          marginX: ["3", "3", "4", "5"],
+          fontWeight: "900",
         }}
       >
-        <Card
-          heading={"Tot 15 keer sneller dan WordPress"}
-          text={`Wetenschappelijke studies hebben keer op keer bewezen dat veel gebruikers
-             afhaken als een website te lang laadt. Een flyfi app laadt tot 10 keer 
-             sneller dan een gewone website. Met de optimale performance van flyfi 
-             webapps kunt u uw doelen sneller bereiken.`}
-        />
-        <Card
-          heading={"Optimale gebruikerservaring"}
-          text={`Flyfi sites zijn zo ontworpen en gebouwt dat de gebruikerservaring lijkt op 
-          die van een app, net zoals gmail of de twitter site. Een betere gebruikerservaring 
-          leidt tot een betere conversie en dus tot meer klanten voor uw bedrijf.`}
-        />
-        <Card
-          heading={"Verbeterde vindbaarheid"}
-          text={`Snelheid en gebruikerservaring zijn factoren die een steeds grotere rol 
-          spelen in Google’s zoekresultaten. Een moderne webapp van flyfi kan zo niet alleen 
-          der conversie verhogen, maar ook het bezoekersaantal van uw webapp.`}
-        />
-        <Card
-          heading={"Veilig zonder onderhoud"}
-          text={`Anders dan een WordPress site hoeft een flyfi app niet onderhouden te 
-          worden. Er zijn geen veiligheidsrisico’s zoals gehackte of verouderde plugins en geen 
-          noodzaak voor updates. Dit bespaart u kosten en zorgen, zodat u kunt focussen op 
-          uw onderneming.`}
-        />
-        <Card
-          heading={"Geen of zeer lage vaste kosten"}
-          text={`Met uitzondering van bvb. webshops zijn er aan flyfi apps geen vaste kosten 
-          verbonden. Voor de hosting werken wij samen met de supersnelle cloud hosting van 
-          Netlify. Voor de allermeeste bedrijven is het gratis pakket voldoende.`}
-        />
-        <Card
-          heading={"Klaar voor de toekomst"}
-          text={`Vanaf volgend jaar introduceert Google de Core Web Vitals, een manier om de 
-          gebruikerservaring te verwerken in de zoekresultaten. Met een flyfi app bent u nu 
-          al voorbereid op de toekomst en zult u vanaf volgend jaar extra profiteren.`}
-        />
-      </Grid>
+        53% van alle bezoekers haakt af als een site langer dan 3 seconden
+        laadt.
+      </motion.h3>
     </React.Fragment>
   )
 }
