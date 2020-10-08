e39b43723872dcc9fcc13aa8e63647ad0558c14b
heinerbehrends <heinerbehrends@hotmail.com>
2020-10-08 16:10:24 +0200

    basic contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+import ContactForm from "../components/Contact-form"
+// import ContactIcons from ".../components/Contact-icons"
+
+export default function Contact() {
+  return (
+    <Layout>
+      <ContactForm />
+    </Layout>
+  )
+}
