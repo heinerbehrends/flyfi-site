f183ec6332b73ad685e766864fdfc70c63d94bcb
heinerbehrends <heinerbehrends@hotmail.com>
2020-10-14 14:05:36 +0200

    Added grid layout, ContactIcons component

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,27 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import React from "react"
 import Layout from "../components/Layout"
 import ContactForm from "../components/Contact-form"
-// import ContactIcons from ".../components/Contact-icons"
+import ContactIcons from "../components/Contact-icons"
 
 export default function Contact() {
   return (
     <Layout>
-      <ContactForm />
+      <section
+        sx={{
+          display: "flex",
+          marginTop: ["5", "6"],
+          flexWrap: "wrap",
+          width: "100%",
+          marginX: "auto",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ContactForm />
+        <ContactIcons />
+      </section>
     </Layout>
   )
 }
