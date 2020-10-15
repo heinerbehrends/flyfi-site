80a3b94bb8fd25f0d6cc1bde816e2109d46aae9e
heinerbehrends <heinerbehrends@hotmail.com>
2020-10-15 21:28:45 +0200

    Added reset.css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
-require("typeface-fira-sans")
+import "typeface-fira-sans"
+import "./src/reset.css"
+// import React from "react"
+// import Layout from "./src/components/Layout"
+
+// export function wrapPageElement({ element, props }) {
+//   return <Layout {...props}>{element}</Layout>
+// }
