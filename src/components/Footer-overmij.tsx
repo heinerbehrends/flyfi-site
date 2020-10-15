050120c2c5d55371bc3ad1eee8f7dcf49db01152
heinerbehrends <heinerbehrends@hotmail.com>
2020-10-15 23:34:45 +0200

    Added gatsby-image

--- src/components/Footer-overmij.tsx
+++ src/components/Footer-overmij.tsx
@@ -1,13 +1,31 @@
 /** @jsx jsx */
 import { jsx, Styled, Box, Flex } from "theme-ui"
 import React from "react"
-import portret from "../jpgs/portret-01-small.jpg"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
 export default function FooterOverMij() {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      file(relativePath: { eq: "portret-01-small.jpg" }) {
+        childImageSharp {
+          id
+          fixed(width: 120) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  console.log(data.file.childImageSharp.fixed)
   return (
     <Box sx={{ marginX: ["0", "4"] }}>
       <Flex sx={{ alignItems: "center" }}>
-        <img sx={{ borderRadius: "50%" }} src={portret} />
+        <Img
+          sx={{ borderRadius: "50%" }}
+          fixed={data.file.childImageSharp.fixed}
+          alt={"a recent portret of me"}
+        />
         <Styled.h3 sx={{ fontSize: "5", marginLeft: "3" }}>Over mij</Styled.h3>
       </Flex>
       <p sx={{ lineHeight: "body", marginTop: "3" }}>
