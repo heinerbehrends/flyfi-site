1bff3687fcc98e840e1924866b715f866d2fbbe1
heinerbehrends <heinerbehrends@hotmail.com>
2020-10-26 11:51:27 +0100

    Fixed Accordion Animation

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,68 +1,58 @@
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui"
-import React, { useState } from "react"
-import { motion } from "framer-motion"
+import * as React from "react"
+import { motion, AnimatePresence } from "framer-motion"
 import PlusIcon from "../icons/add_circle_outline-24px.svg"
 import MinIcon from "../icons/remove_circle_outline-24px.svg"
 
-type faqT = {
-  vraag: string
-  antwoord: string
-}
-
-export default function Accordion({ items }) {
-  const [openIndex, setOpenIndex] = useState(0)
-  function isOpen(id, openIndex) {
-    return id === openIndex
-  }
-  function setIndex(id, openIndex) {
-    if (isOpen(id, openIndex)) {
-      setOpenIndex(0)
-    } else setOpenIndex(id)
-  }
-  function AccordionItem({ vraag, antwoord, id }) {
-    return (
-      <div sx={{ height: `${isOpen(id, openIndex) ? "auto" : "62px"}` }}>
-        <button
-          aria-controls={`accordion-panel-0${id}`}
-          aria-expanded={isOpen(id, openIndex) ? true : false}
-          id={`accordion-button-0${id}`}
-          onClick={() => setIndex(id, openIndex)}
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            maxWidth: "inherit",
-            backgroundColor: "#fff",
-            width: "100%",
-            border: "none",
-            fontSize: "4",
-            fontFamily: "fira sans",
-            paddingX: "3",
-            paddingY: "3",
-          }}
-        >
-          <h2 sx={{ width: "66ch", textAlign: "left" }}>{vraag}</h2>
-          {isOpen(id, openIndex) ? <MinIcon /> : <PlusIcon />}
-        </button>
-        <Styled.p
-          id={`accordion-panel-0${id}`}
-          aria-labelledby={`accordion-button-0${id}`}
-          sx={{
-            paddingX: "3",
-            display: `${isOpen(id, openIndex) ? "block" : "none"}`,
-          }}
-        >
-          {antwoord}
-        </Styled.p>
-      </div>
-    )
-  }
+export default function Accordion({ i, expanded, setExpanded, faq }) {
+  const isOpen = i === expanded
 
   return (
-    <section sx={{ maxWidth: "66ch", marginTop: "4" }}>
-      {items.map((faq: faqT, i: number) => (
-        <AccordionItem vraag={faq.vraag} antwoord={faq.antwoord} id={i + 1} />
-      ))}
-    </section>
+    <React.Fragment>
+      <motion.button
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          maxWidth: "inherit",
+          width: "60ch",
+          backgroundColor: "inherit",
+          border: "none",
+          fontSize: "4",
+          fontFamily: "fira sans",
+          fontWeight: "bold",
+          paddingX: "3",
+          paddingY: "3",
+          borderRadius: "3",
+          "&:focus": {
+            outline: "none",
+            boxShadow: "focus",
+          },
+        }}
+        initial={false}
+        onClick={() => setExpanded(isOpen ? false : i)}
+      >
+        <h2>{faq.vraag}</h2>
+        {isOpen ? <MinIcon /> : <PlusIcon />}
+      </motion.button>
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.section
+            key="content"
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            sx={{ overflow: "hidden" }}
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+          >
+            <Styled.p sx={{ paddingX: "3" }}>{faq.antwoord}</Styled.p>
+          </motion.section>
+        )}
+      </AnimatePresence>
+    </React.Fragment>
   )
 }
--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,7 +1,28 @@
 /** @jsx jsx */
-import { jsx, Styled, Box, Flex } from "theme-ui"
-import React from "react"
+import { jsx, Styled } from "theme-ui"
+import * as React from "react"
+import { useState } from "react"
 import Accordion from "../components/Accordion"
+
+export default function FAQs() {
+  const [expanded, setExpanded] = useState<false | number>(0)
+
+  return (
+    <React.Fragment>
+      <Styled.h1>Vaak gestelde vragen</Styled.h1>
+      <main sx={{ maxWidth: "60ch", marginX: "auto" }}>
+        {faqs.map((faq, i) => (
+          <Accordion
+            i={i}
+            faq={faq}
+            expanded={expanded}
+            setExpanded={setExpanded}
+          />
+        ))}
+      </main>
+    </React.Fragment>
+  )
+}
 
 const faqs = [
   {
@@ -15,12 +36,12 @@ const faqs = [
   },
   {
     vraag: "Hoe maak je een site sneller?",
-    antwoord: `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    antwoord: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
   eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-     proident, sunt in culpa qui officia deserunt mollit anim id est laborum."`,
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
   },
   {
     vraag: "Hoe worden de foto's geoptimaliseerd?",
@@ -32,12 +53,3 @@ const faqs = [
     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
   },
 ]
-
-export default function FAQ() {
-  return (
-    <Flex sx={{ flexDirection: "column", alignItems: "center" }}>
-      <Styled.h1>Vaak gestelde vragen</Styled.h1>
-      <Accordion items={faqs} />
-    </Flex>
-  )
-}
