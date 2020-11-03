4b348bab53456d2fd415dfa807908cbe96c57ea7
heinerbehrends <heinerbehrends@hotmail.com>
2020-11-03 15:20:49 +0100

    Added .png

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,7 @@
 declare module "*.gif"
 declare module "*.mp4"
 declare module "*.webm"
+declare module "*.png"
 declare module "*.svg" {
   import React = require("react")
   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
