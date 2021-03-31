a665a3b5e5b582e1f0cd2584de27441030709831
heinerbehrends <heinerbehrends@hotmail.com>
2021-03-31 11:39:25 +0200

    Changed svg module declaration

--- global.d.ts
+++ global.d.ts
@@ -4,7 +4,7 @@ declare module "*.webm";
 declare module "*.png";
 declare module "*.svg" {
   import React = require("react");
-  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   const src: string;
   export default src;
 }
