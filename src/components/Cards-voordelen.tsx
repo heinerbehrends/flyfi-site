be0161b522ebc6e14203e4da7e6a8b3098b9a221
heinerbehrends <heinerbehrends@hotmail.com>
2021-03-31 11:44:30 +0200

    added fragment pragma, added links, improved text

--- src/components/Cards-voordelen.tsx
+++ src/components/Cards-voordelen.tsx
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+/** @jsxFrag React.Fragment */
 import { jsx } from "theme-ui";
 import React from "react";
 import Card from "./Card";
@@ -25,31 +26,34 @@ export default function CardsVoordelen() {
 
 const cardsVoordelen = [
   {
-    heading: "Tot 15 keer sneller dan WordPress",
+    heading: "Tot 15 keer sneller dan Wordpress",
     text: (
-      <React.Fragment>
-        Wetenschappelijke studies hebben keer op keer bewezen dat veel
-        gebruikers afhaken als een website te lang laadt. Een flyfi app laadt
-        tot 15 keer sneller dan een WordPress website. Met de optimale
-        performance van flyfi webapps kunt u uw doelen sneller bereiken.
-      </React.Fragment>
+      <>
+        <a href="https://hostingtribunal.com/blog/how-speed-affects-website/">
+          Onderzoek van bedrijven
+        </a>{" "}
+        heeft keer op keer bewezen dat veel gebruikers afhaken als een website
+        te lang laadt. Een flyfi app laadt tot 15 keer sneller dan een WordPress
+        website. Met de optimale performance van flyfi webapps kunt u uw doelen
+        sneller bereiken.
+      </>
     ),
   },
   {
     heading: "Optimale gebruikerservaring",
     text: (
-      <React.Fragment>
+      <>
         flyfi sites zijn zo ontworpen en gebouwt dat de gebruikerservaring lijkt
         op die van een app, net zoals gmail of de twitter site. Een betere
         gebruikerservaring leidt tot een betere conversie en dus tot meer
         klanten voor uw bedrijf.
-      </React.Fragment>
+      </>
     ),
   },
   {
     heading: "Verbeterde vindbaarheid",
     text: (
-      <React.Fragment>
+      <>
         <a href="https://webmasters.googleblog.com/2018/01/using-page-speed-in-mobile-search.html">
           Snelheid
         </a>{" "}
@@ -61,40 +65,43 @@ const cardsVoordelen = [
         zijn factoren die een steeds grotere rol spelen in Google’s
         zoekresultaten. Een moderne webapp van flyfi kan zo niet alleen de
         conversie verhogen, maar ook het bezoekersaantal van uw webapp.
-      </React.Fragment>
+      </>
     ),
   },
   {
     heading: "Veilig zonder onderhoud",
     text: (
-      <React.Fragment>
+      <>
         Anders dan een WordPress site hoeft een flyfi app niet onderhouden te
         worden. Er zijn geen veiligheidsrisico’s zoals gehackte of verouderde
         plugins en geen noodzaak voor updates. Dit bespaart u kosten en zorgen,
         zodat u kunt focussen op uw onderneming.
-      </React.Fragment>
+      </>
     ),
   },
   {
-    heading: "Geen of zeer lage vaste kosten",
+    heading: "Bijzonder lage vaste kosten",
     text: (
-      <React.Fragment>
-        Met uitzondering van bvb. webshops zijn er aan flyfi apps geen vaste
-        kosten verbonden. Voor de hosting werken wij samen met de supersnelle
-        cloud hosting van Netlify. Voor de allermeeste bedrijven is het gratis
-        pakket voldoende.
-      </React.Fragment>
+      <>
+        Voor de hosting werken wij samen met de supersnelle cloud hosting van{" "}
+        <a href="https://www.netlify.com/">Netlify</a>. Voor de allermeeste
+        bedrijven is het gratis pakket voldoende. Sites met veel verkeer betalen
+        alleen voor de hoeveelheid data die ze ook werkelijk gebruiken.
+      </>
     ),
   },
   {
     heading: "Progressive Web Apps",
     text: (
-      <React.Fragment>
-        flyfi's progressive web apps zijn een geschikt alternatief voor de
-        ontwikkeling van Android en iOS apps. Ze zijn beter vindbaar,
-        makkelijker te installeren en te updaten, gunstiger in de ontwikkeling
-        en verhogen de conversie.
-      </React.Fragment>
+      <>
+        flyfi's{" "}
+        <a href="https://themanifest.com/mobile-apps/should-you-consider-investing-progressive-web-app">
+          Progressive Web Apps{" "}
+        </a>
+        zijn een geschikt alternatief voor de ontwikkeling van Android en iOS
+        apps. Ze zijn beter vindbaar, makkelijker te installeren en te updaten,
+        gunstiger in de ontwikkeling en verhogen de conversie.
+      </>
     ),
   },
 ];
