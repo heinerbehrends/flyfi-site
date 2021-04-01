b1a6f9029d39858c380990a1f5bf7eae67eddecb
heinerbehrends <heinerbehrends@hotmail.com>
2021-04-01 20:18:43 +0200

    updated jest-emotion to @emotion/jest

--- setup-test-env.js
+++ setup-test-env.js
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
-import { createSerializer } from "jest-emotion";
+import { createSerializer } from "@emotion/jest";
 import * as emotion from "@emotion/core";
 
 expect.addSnapshotSerializer(createSerializer(emotion));
