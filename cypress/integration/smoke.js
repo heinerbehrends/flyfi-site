0cac9e2b2dc6a73f901593c79e60d2c41e780de3
heinerbehrends <heinerbehrends@hotmail.com>
2021-04-26 16:37:23 +0200

    improved smoke test to visit all pages

--- cypress/integration/smoke.js
+++ cypress/integration/smoke.js
@@ -2,6 +2,13 @@ describe("app", () => {
   it("works", () => {
     cy.visit("/");
     cy.findByText(/about/i).click();
-    cy.findByText(/over flyfi en mij/i);
+    cy.location("pathname").should("match", /about/i);
+    cy.findByText(/over flyfi en mij/i).should("be.visible");
+    cy.findAllByText(/faq/i).click({ multiple: true });
+    cy.location("pathname").should("match", /faq/i);
+    cy.findAllByText(/vaak gestelde vragen/i).should("be.visible");
+    cy.findAllByRole("link", { name: /contact/i }).click({ multiple: true });
+    cy.location("pathname").should("match", /contact/i);
+    cy.findByText(/verstuur/i);
   });
 });
