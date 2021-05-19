c8cc75165594dfbfe616b0a12b0809e10ad28e34
heinerbehrends <heinerbehrends@hotmail.com>
2021-05-19 13:02:30 +0200

    Removed image

--- src/components/Footer-overmij.tsx
+++ src/components/Footer-overmij.tsx
@@ -1,34 +1,15 @@
 /** @jsx jsx */
 import { jsx, Themed } from "theme-ui";
 import { graphql, Link, useStaticQuery } from "gatsby";
-import Img from "gatsby-image";
 import { footerOverMijText } from "../data/aboutData";
 
 export default function FooterOverMij() {
-  const data = useStaticQuery(graphql`
-    query FooterQuery {
-      file(relativePath: { eq: "portret-01-small.jpg" }) {
-        childImageSharp {
-          id
-          fixed(width: 120) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `);
   return (
-    <div sx={{ marginX: ["0", "4"] }}>
-      <div sx={{ display: "flex", alignItems: "center" }}>
-        <Img
-          sx={{ borderRadius: "50%" }}
-          fixed={data.file.childImageSharp.fixed}
-          alt={"a recent portret of me"}
-        />
+    <article sx={{ marginX: ["0", "4"], marginTop: "4" }}>
+      <h3>
         <Link
           to={"/about"}
           sx={{
-            paddingY: 2,
             textDecoration: "none",
             "&:hover": { textDecoration: "underline" },
             "&:focus": {
@@ -37,17 +18,17 @@ export default function FooterOverMij() {
             },
           }}
         >
-          <Themed.h3 sx={{ fontSize: "5", marginLeft: "3", color: "#333" }}>
+          <Themed.h3 sx={{ fontSize: "5", color: "#333", marginLeft: 0 }}>
             Over mij
           </Themed.h3>
         </Link>
-      </div>
+      </h3>
       <p sx={{ lineHeight: "body", marginTop: "3" }}>
         {footerOverMijText}
         <br />
         <br />
         <p>© Heiner S. Behrends, {new Date().getFullYear()}</p>
       </p>
-    </div>
+    </article>
   );
 }
